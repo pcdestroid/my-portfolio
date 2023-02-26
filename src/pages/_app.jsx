0088f4e76058db8a3798fb7ab97d09a01ad698e8
pcdestroid <alanfrancisangra@gmail.com>
2023-02-26 15:10:55 -0300

    teste

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,13 +1,11 @@
-import '/src/styles/globals.css'
-import Music from '/src/components/music.jsx'
-import Headpage from '/src/components/Headpage.jsx'
-import Menu from '/src/components/Menu.jsx'
-import Animation from '/src/components/Animation.jsx'
+import '@/styles/globals.css'
+import Music from '@/components/music.jsx'
+import Menu from '@/components/Menu.jsx'
+import Animation from '@/components/Animation.jsx'
 
 export default function App({ Component, pageProps }) {
   return <>
     <nav className='menu'><Menu /><Music /></nav>
-    <Headpage />
     <Component {...pageProps} />
     {Animation()}
   </>
