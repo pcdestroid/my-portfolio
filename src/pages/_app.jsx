fc1c551dd546bb6bdbdbb5c67952d0a454b9efe2
pcdestroid <alanfrancisangra@gmail.com>
2023-02-26 14:28:56 -0300

    correções

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,8 +1,8 @@
-import '@/styles/globals.css'
-import Music from '@/components/music'
-import Headpage from '@/components/Headpage'
-import Menu from '@/components/Menu'
-import Animation from '@/components/Animation'
+import '/src/styles/globals.css'
+import Music from '/src/components/music'
+import Headpage from '/src/components/Headpage'
+import Menu from '/src/components/Menu'
+import Animation from '/src/components/Animation'
 
 export default function App({ Component, pageProps }) {
   return <>
