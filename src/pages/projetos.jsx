5b5410766ea9ae9d31759f461da24052088ad748
pcdestroid <alanfrancisangra@gmail.com>
2023-03-20 19:04:50 -0300

    add projeto speak-english

--- src/pages/projetos.jsx
+++ src/pages/projetos.jsx
@@ -15,8 +15,12 @@ export default function Home() {
             </article>
 
             <div className="font-exo projetos">
+                <a href="https://chat-gpt-por-meio-de-comandos-de-voz.vercel.app/" target={'_blank'}>CHATGPT by voice</a>
                 <a href="https://chat-neon-six.vercel.app/?sala=Mundo&chave=Bitcoin" target={'_blank'}>CriptoChat</a>
                 <a href="https://pcdestroid-login-com-senha-e-cadastro.vercel.app/" target={'_blank'}>Login & Register</a>
+                <a href="https://speak-english-black.vercel.app/" target={'_blank'}>Speak English</a>
+
+
             </div>
 
         </>
