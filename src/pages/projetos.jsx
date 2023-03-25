4c07c852350c8cc5eaebd3d6dc8a63a0d4a124d5
pcdestroid <alanfrancisangra@gmail.com>
2023-03-25 17:23:51 -0300

    add project Create SVG From Image

--- src/pages/projetos.jsx
+++ src/pages/projetos.jsx
@@ -15,12 +15,11 @@ export default function Home() {
             </article>
 
             <div className="font-exo projetos">
+                <a href="https://create-svg-from-png-jpg-webp-bmp-gif-avif.vercel.app/" target={'_blank'}>Create SVG From Images</a>
                 <a href="https://chat-gpt-por-meio-de-comandos-de-voz.vercel.app/" target={'_blank'}>CHATGPT by voice</a>
                 <a href="https://chat-neon-six.vercel.app/?sala=Mundo&chave=Bitcoin" target={'_blank'}>CriptoChat</a>
                 <a href="https://pcdestroid-login-com-senha-e-cadastro.vercel.app/" target={'_blank'}>Login & Register</a>
                 <a href="https://speak-english-black.vercel.app/" target={'_blank'}>Speak English</a>
-
-
             </div>
 
         </>
